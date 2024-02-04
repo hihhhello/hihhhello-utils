d5a910f3df09e7a64e05a85a2485299865c9702e
hihhhello <grigori.ratushniak@gmail.com>
2024-02-04 11:56:02 -0600

    remove useLoading

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -2,45 +2,6 @@ import { debounce } from 'lodash';
 import { useCallback, useLayoutEffect, useMemo, useState } from 'react';
 import { Id, toast } from 'react-toastify';
 
-export const useLoadingToast = () => {
-  const showLoading = useCallback((message: string) => {
-    return toast.loading(message);
-  }, []);
-
-  const handleSuccess = useCallback(
-    ({ message, toastId }: { toastId: Id; message: string }) => {
-      toast.update(toastId, {
-        render: message,
-        type: 'success',
-        autoClose: 2500,
-        isLoading: false,
-      });
-    },
-    [],
-  );
-
-  const handleError = useCallback(
-    ({ message, toastId }: { toastId: Id; message: string }) => {
-      toast.update(toastId, {
-        render: message,
-        type: 'error',
-        autoClose: 2500,
-        isLoading: false,
-      });
-    },
-    [],
-  );
-
-  return useMemo(
-    () => ({
-      showLoading,
-      handleSuccess,
-      handleError,
-    }),
-    [handleError, handleSuccess, showLoading],
-  );
-};
-
 export const createUseLoadingToast = (toastFn: typeof toast) => {
   'use client';
 
